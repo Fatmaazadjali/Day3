17d8e4e7f2375ae7decfaa9340b6f88c3bd404a9
Fatmahzadjali_1@hotmail.com <maathermohammed77@gmail.com>
2023-11-15 15:13:00 +0400

    prefix postfix

---
+++ Day3.solution/Day3/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace Day3
+{
+	 class Program
+	{
+		static void Main()
+		{
+			int y = 10;
+			y = +1;
+			String result = (y % 2 == 0) ? "y is even" : "y is odd";
+			Console.WriteLine(result);
+
+			int x = 970;
+			x -= 1;
+
+			String result1 = (x % 3 == 0) ? "x is divisable to 3" : " x is not divisable to3";
+			Console.WriteLine($"the result is {result1}");
+		}
+	}
+}
